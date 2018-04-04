e8fab6db4af2918b9a607c0f427b724b7f985b43
BLaurenB <blbillington1@gmail.com>
2018-04-03 18:27:08 -0600

    Adds tests for user, traits, and mate_preferences tables

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -1,12 +1,12 @@
 class Trait < ApplicationRecord
   belongs_to :user
 
-  enum body_type %w(any athletic average beefy curvy extra_curvy large slender)
-  enum smoker %w(never socially smoker)
-  enum alcohol %w(never socially drinker )
+  enum body_type: [:any, :athletic, :average, :beefy, :curvy, :extra_curvy, :large, :slender], _prefix: true
+  enum smoker: [:never, :socially, :smoker], _prefix: true
+  enum alcohol: [:never, :socially, :drinker], _prefix: true
   # religiosity -- just self rate 1-3, where 0 is no pref
   # politicalness -- just self rate 1-3, where 0 is no pref
-  enum has_pets ( "none", "dog(s)" , "cats(s)", "other", "allergeric")
-  enum orientation %w(straight queer)
-  enum column %w(male female non_binary)
+  enum has_pets: [ :none, :dog, :cat, :other, :allergeric], _prefix: true
+  enum orientation: [:straight, :queer], _prefix: true
+  enum gender: [:male, :female, :non_binary], _prefix: true
 end
--- spec/factories/mate_preferences.rb
+++ spec/factories/mate_preferences.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :mate_preference do
-    user nil
+    user 1
     min_height 60
     max_height 70
     smoker 1
-    alcohol 1
+    alcohol 0
     ethnicity "unknown"
     education false
-    body_type "curvy"
-    zip_radius 1
-    religion "MyString"
+    # body_type ["curvy", "beefy", "slender"]
+    zip_radius 25
+    religion "Some Religion"
     religiosity 1
-    political_leaning 1
+    political_leaning 0
     politicalness 1
-    has_kids false
+    has_kids true
     wants_kids 1
-    has_pets 1
-    orientation 1
+    has_pets 0
+    orientation 0
     gender 1
   end
 end
