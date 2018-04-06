b123415b9504b70ca759febae459a88fb37e0d70
BLaurenB <blbillington1@gmail.com>
2018-04-06 17:57:55 -0600

    Adds some API endpoints. Call call daters, daters/:id/backers, and the same for backers

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,33 @@ Rails.application.routes.draw do
   resources :search, only: [:index]
 
   root to: "homes#show"
-  
+
   resources :conversations do
     resources :messages
    end
 
-  resources :dashboard 
-  
-  
+  resources :dashboard
+
+
   resources :users
   get 'user/:id/pool', to: 'pool#index' , as: "user_pool"
 
+  namespace :api do
+    namespace :v1 do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      resources :daters, only: [:index, :show]
+      namespace :daters do
+        get "/:id/backers", to: "backers#index"
+      end
+
+      resources :backers, only: [:index, :show]
+      namespace :backers do
+        get "/:id/daters", to: "daters#index"
+      end
+      
+    end
+  end
+
+  # get '/api/v1/daters/:id/backers', to: '/api/v1/daters/backers#index'
+  # For details on the DSL available within this file, see http://guides.rubyonraiils.org/routing.html
 end
