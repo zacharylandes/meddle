b0a3c9bc2897de038aa0bdc833eafbb28edca8b6
BLaurenB <blbillington1@gmail.com>
2018-04-07 19:35:30 -0600

    working posting daterbacker relationship via POST

--- app/controllers/api/v1/daters/backers_controller.rb
+++ app/controllers/api/v1/daters/backers_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::Daters::BackersController < ApplicationController
     render json: Dater.find(params[:id]).backers
   end
 
+  def update
+    binding.pry
+    current_dater.backers << Backer.find(params[:id])
+  end
+
 #don't need a show, just make a call to the actual backers endpoint if needed
+
+
+  private
+
 
 end
