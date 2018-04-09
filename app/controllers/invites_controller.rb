a32501bbef1f30d3b02b28e904269317b2c36f33
BLaurenB <blbillington1@gmail.com>
2018-04-08 19:21:12 -0600

    invites

---
+++ app/controllers/invites_controller.rb
@@ -0,0 +1,18 @@
+class InvitesController < ApplicationController
+
+  def show
+    # binding.pry
+    if params[:email]
+      UserMailer.dater_requests_backer(params[:email])
+      redirect_to "/dashboard/#{current_user.id}?tab=dater"
+
+    elsif params[:f_name]
+
+      redirect_to dashboard_path(current_user)
+    else
+      render file: 'public/404.html'
+    end
+
+  end
+
+end
