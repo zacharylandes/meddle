0bc908549f720a6c9b5de22c1d0690a2e05994f6
BLaurenB <blbillington1@gmail.com>
2018-04-08 19:26:43 -0600

    user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,13 @@
 class UserMailer < ApplicationMailer
 
-  def dater_requests_backer(user)
-    @user = user
-    @url  = 'http://localhost:3000/'
-    mail(to: @user.email, subject: '#{@user.f_name} would like to connect with you!')
+  def dater_requests_backer(email)
+
+    @url  = "" #this link should hav some sort of link that takes the user to the site and connects them automatically to the dater
+    mail(to: email, subject: "#{@current_user.f_name} would like to connect with you!")
   end
 
   def backer_requests_dater(user)
     @user = user
-    @url  = 'http://localhost:3000/'
     mail(to: @user.email, subject: '#{@user.f_name} would like to connect with you!')
   end
 
