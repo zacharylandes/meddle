aece3179d5263223e169c5ccdc07d1648ba4b979
ZacharyLandes <zach.g.landes@gmail.com>
2018-04-09 21:56:07 -0600

    modals

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ class Seed
 
   def run
 
-    shots = ['https://s3.amazonaws.com/pytdating/admin-pic2.jpg','https://s3.amazonaws.com/pytdating/admin-pic3.jpg','https://s3.amazonaws.com/pytdating/bill.jpg','https://s3.amazonaws.com/pytdating/blackwhite.jpg','https://s3.amazonaws.com/pytdating/car-draw.png','https://s3.amazonaws.com/pytdating/fat-bunny.jpg','https://s3.amazonaws.com/pytdating/hamster1.jpg','https://s3.amazonaws.com/pytdating/ilana.jpg','https://s3.amazonaws.com/pytdating/leopard-onesie.png','https://s3.amazonaws.com/pytdating/mulder.jpg','https://s3.amazonaws.com/pytdating/skully.jpg']
+    shots = ['https://s3.amazonaws.com/pytdating/p2.jpeg','https://s3.amazonaws.com/pytdating/p3.jpeg','https://s3.amazonaws.com/pytdating/p4.jpeg','https://s3.amazonaws.com/pytdating/p1.jpeg','https://s3.amazonaws.com/pytdating/p7.jpeg','https://s3.amazonaws.com/pytdating/p11.jpeg','https://s3.amazonaws.com/pytdating/12.jpeg','https://s3.amazonaws.com/pytdating/13.jpeg']
 
-    user_1 = User.create!( id: 100, provider: "google_oauth2", uid: "12345", auth_name: "Susie", oauth_token: "tokenhere", oauth_expires_at: "2018-04-04 20:19:18", created_at: "2018-04-04 19:19:19", updated_at: "2018-04-04 19:19:19", f_name: "Susie", l_name:"Smith", email: "email1@email.com", remote_image_url: shots.sample )
+    user_1 = User.create!( id: 100, provider: "google_oauth2", uid: "12345", auth_name: "Susie", oauth_token: "tokenhere", oauth_expires_at: "2018-04-04 20:19:18", created_at: "2018-04-04 19:19:19", updated_at: "2018-04-04 19:19:19", f_name: "Susie", l_name:"Smith", email: "email1@email.com", remote_image_url: shots[0] )
     dater_1 = Dater.create!(id: user_1.id, user_id: user_1.id,f_name: user_1.f_name, l_name: user_1.l_name)
     backer_1 = Backer.create!(id: user_1.id, user_id: user_1.id, f_name: user_1.f_name, l_name: user_1.l_name)
 
@@ -44,7 +44,7 @@ class Seed
     dater_6 = Dater.create!(id: user_6.id, user_id: user_6.id,f_name: user_6.f_name, l_name: user_6.l_name)
     backer_6 = Backer.create!(id: user_6.id, user_id: user_6.id, f_name: user_6.f_name, l_name: user_6.l_name)
 
-    user_7 = User.create!( id: 700, provider: "google_oauth2", uid: "123359", auth_name: "Mr.Match", oauth_token: "tokenhere", oauth_expires_at: "2018-04-04 20:19:18", created_at: "2018-04-04 19:19:19", updated_at: "2018-04-04 19:19:19", f_name: "Stack", l_name:"Steiner", email: "email7@email.com", remote_image_url: shots.sample )
+    user_7 = User.create!( id: 700, provider: "google_oauth2", uid: "123359", auth_name: "Mr.Match", oauth_token: "tokenhere", oauth_expires_at: "2018-04-04 20:19:18", created_at: "2018-04-04 19:19:19", updated_at: "2018-04-04 19:19:19", f_name: "Stack", l_name:"Steiner", email: "email7@email.com", remote_image_url: shots[1] )
     dater_7 = Dater.create!(id: user_7.id, user_id: user_7.id,f_name: user_7.f_name, l_name: user_7.l_name)
     backer_7 = Backer.create!(id: user_7.id, user_id: user_7.id, f_name: user_7.f_name, l_name: user_7.l_name)
 
@@ -61,10 +61,10 @@ class Seed
     dater_2.backers << backer_4
 
 
-    comment_1 = Comment.create!(dater_id: dater_1.id, backer_id: backer_1.id, comment: "this is from the first backer")
-    comment_2 = Comment.create!(dater_id: dater_1.id, backer_id: backer_2.id, comment: "this is from SECOND BACKER")
-    comment_3 = Comment.create!(dater_id: dater_2.id, backer_id: backer_3.id, comment: "this is from third backer")
-    comment_4 = Comment.create(dater_id: dater_2.id, backer_id: backer_4.id, comment: "this is from fourth backer")
+    # comment_1 = Comment.create!(dater_id: dater_1.id, backer_id: backer_1.id, comment: "this is from the first backer")
+    # comment_2 = Comment.create!(dater_id: dater_1.id, backer_id: backer_2.id, comment: "this is from SECOND BACKER")
+    # comment_3 = Comment.create!(dater_id: dater_2.id, backer_id: backer_3.id, comment: "this is from third backer")
+    # comment_4 = Comment.create(dater_id: dater_2.id, backer_id: backer_4.id, comment: "this is from fourth backer")
 
 
     dater_1_traits = Trait.create!(dater_id: dater_1.id, height: 60, smoker: 0, alcohol: 0, ethnicity: 0, education: true, body_type: 0, occupation: 0, zip: 80203, religion: 0, religiosity: 1, political_leaning: 2, politicalness: 3, has_kids: false, wants_kids: false, has_pets: false, orientation: 1, gender: 1, other: "", created_at: 0, updated_at: 0)
@@ -82,9 +82,9 @@ class Seed
     dater_1_pool = Match.create!(dater_id: dater_1.id, match_id: match.id)
 
 
-    backer_eval_1 = BackerEval.create!(dater_id: dater_1.id, backer_id: backer_2.id, q_1: "this is an amazing answer about a person")
-    backer_eval_1 = BackerEval.create!(dater_id: dater_1.id, backer_id: backer_2.id, q_1: "this is also an amazing answer about a person")
-    backer_eval_1 = BackerEval.create!(dater_id: dater_2.id, backer_id: backer_3.id, q_1: "this is also also an amazing answer about a person")
+    # backer_eval_1 = BackerEval.create!(dater_id: dater_1.id, backer_id: backer_2.id, q_1: "this is an amazing answer about a person")
+    # backer_eval_1 = BackerEval.create!(dater_id: dater_1.id, backer_id: backer_2.id, q_1: "this is also an amazing answer about a person")
+    # backer_eval_1 = BackerEval.create!(dater_id: dater_2.id, backer_id: backer_3.id, q_1: "this is also also an amazing answer about a person")
 
   end
 
