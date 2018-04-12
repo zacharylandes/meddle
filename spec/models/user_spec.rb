25668080127e8bddf0fc526ef42afbb4c38f6ba2
BLaurenB <blbillington1@gmail.com>
2018-04-12 16:25:40 -0600

    wip

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,20 @@ RSpec.describe User, type: :model do
 
       expect(user.id).to eq(User.last.id)
     end
-  end 
+  end
+
+  context "User responds to other tables" do
+
+    it "responds to " do
+      user = build(:user)
+      expect(user).to respond_to(:daters)
+      expect(user).to respond_to(:backers)
+      expect(user).to respond_to(:dater_backers)
+      expect(user).to respond_to(:mate_preference)
+      expect(user).to respond_to(:trait)
+      expect(user).to respond_to(:trait)
+    end
+  end
+
+
 end
