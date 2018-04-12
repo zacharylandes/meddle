0b0c6764b0509f684ba08f317cb8c5a999855fbb
BLaurenB <blbillington1@gmail.com>
2018-04-12 16:25:40 -0600

    wip

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,10 @@ FactoryBot.define do
     uid "MyString"
     f_name "MyString"
     l_name 'Mystring'
+    email "email@email.com"
     oauth_token "MyString"
     oauth_expires_at "2018-04-02 14:52:12"
+    remote_image_url =  "https://s3.amazonaws.com/pytdating/p3.jpeg"
+
   end
 end
