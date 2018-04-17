8ca21ef4efb13f37c4a618e95beff3f8a29e7e2b
ZacharyLandes <zach.g.landes@gmail.com>
2018-04-17 14:15:04 -0600

    email flash

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -241,16 +241,15 @@ $('#dater-q5-button').click(function(){
 ////////////////////////////////////////////
 
     // SEND EMAIL TO INVITE A DATER
-    $(".search-for-new-dater").on("click", "#invite-dater-button", function(event) {
+    $("#invite-dater-button").on("click",  function(event) {
       event.preventDefault()
       let email = event.target.offsetParent.childNodes[1].value
-      console.log(email)
-      $.get(`https://pytdating.herokuapp.com/invites?email=${email}&add_dater=true`,()=> {
-        $(".list-and-add-backers").append(`
-          <div class="alert alert-success" role="alert" id="dater-invited-alert">
-            <h4 class="alert-heading"> Email Sent!</h4>
-            `).delay( 1500 ).fadeOut( 300 )
-          })
+      $.get(`https://pytdating.herokuapp.com/invites?email=${email}&add_dater=true`, () => {})
+      $(".backer-tab-body").append(`
+        <div class="alert alert-success" role="alert" id="dater-invited-alert">
+          <h4 class="alert-heading"> Email Sent!</h4>
+          `).delay( 1500 )
+          
     })
 
     // SEND EMAIL TO INVITE NEW BACKER
